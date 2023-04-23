10d6e0e9ff6ef9a0a0247cefe51939c7cabda16a
ljhobson <louishobson5@gmail.com>
2023-04-23 14:43:44 +1200

    modified:   Game.java

--- Game.java
+++ Game.java
@@ -2,7 +2,7 @@ public class Game {
 	public static void main(String[] args) {
 		Game game = new Game();
 		
-		// setup the team
+		// setup the team (get the user invested in the game)
 		// VVV create a Validators class which contains all these validators
 		Validator nameValidator = (String inp) -> {
 			// .. write the team name validator here ...
@@ -25,24 +25,52 @@ public class Game {
 		game.output("Pick a difficulty level");
 		int difficulty = game.options(difficultySettings);
 		
+		// print rules
+		game.output( game.rules() );
+		
 		// for week in weeks
-			// (1) print game stats (including player.stats (which returns money)) (money, current week, and weeks remaining)
+			// (0) print game stats (including player.stats (which returns money)) (money, current week, and weeks remaining)
 			game.output( game.stats() + player.stats() );
-			// (2) go to the club
+			
+			
+			Options actions = new Options(new String[] {"Manage your team at the clubs", "Play Matches", "Go to the shop", "Take a Bye (move to the next week)"} );
+			int action = game.options();
+			
+			// (1) go to the club
 			club.setOptions( ... );
 			
-			int choice = game.options( club.getOptions() );
-			game.output( club.useOption( choice ) );
+			int choice1 = game.options( club.getOptions() );
+			game.output( club.useOption( choice1 ) );
 			
 			club.useItem(item, athlete);
 			// item objects has a function which takes athlete as a parameter then applies effects to athlete.
 			
-			// (3) go to the statium
-			// (4) visit the market
+			// (2) go to the statium
+			Options staduimOptions = new Options( matches )
+			game.output("Matches left you can play");
+			int choice2 = game.options( staduimOptions ).join( "Go back..." );
+			
+			
+			// (3) visit the shop/market
 			game.output("Which Item do you want to buy");
-			game.options( new Options(shop.getItems()) );
-			shop.buy( game.input() );
-			// (5) move to the next week
+			int choice3 = game.options(new Options( shop.getItems() ).join( "Leave shop" )); // see later what types of parameter .join() takes
+			if (shop.canBuy( choice3 )) {
+				Item bought = shop.buy( choice3 );
+				// add bought to inventory
+				player.addItem( bought );
+				game.output("Purchase Successful!");
+			} else {
+				String reason = "you need $" + shop.price( choice3 ) + " for item " + shop.item( choice3 ) + ", you have $" + player.getMoney() + "."; // Item item also has a .getPrice() method
+				game.output("Purchase Unsuccessful... " + reason);
+			}
+			
+			// (4) move to the next week
+			if (choice2 == stadiumOptions.last()) {
+				// take a bye
+				// update classes
+				// random events
+				// train athlete
+			}
 		
 		// display end screen
 		game.output("Stats");
