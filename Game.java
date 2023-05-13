09445ea8c20427ec77be00a2dfd7be8bf2cdb36c
ljhobson <louishobson5@gmail.com>
2023-05-13 12:37:46 +1200

    modified:   Game.java

--- Game.java
+++ Game.java
@@ -88,6 +88,8 @@ public class Game {
 	public void resetMatches(Matches matches, Team player) {
 		Team opponent;
 		
+		matches.clear();
+		
 		opponent = new Team("Diamond Dogs", this.teamSize, this.fieldSize);
 		opponent.addAthlete( new Athlete("Andy", 30, new Stats(new int[] {3, 2, 5})) );
 		opponent.addAthlete( new Athlete("Andy1", 30, new Stats(new int[] {3, 2, 5})) );
@@ -135,6 +137,90 @@ public class Game {
 				String reason = e.getMessage();
 				this.output("Purchase Unsuccessful... " + reason);
 			}
+		}
+	}
+	public void simulateClub(Team player) {
+		Options clubChoiceOptions = new Options(new String[] {"View Team", "View Inventory", "Leave Club"} );
+		this.output("Select an option (Enter " + clubChoiceOptions.last() + " to leave)");
+		int choice = this.options(clubChoiceOptions);
+		
+		if (this.first()) { // view team
+			this.output( player );
+			// swap athletes
+			Options clubOptions = new Options(new String[] {"Swap Athletes", "Leave Club"} );
+			this.output("Select an option (Enter " + clubOptions.last() + " to leave)");
+			choice = this.options(clubOptions);
+			
+			if ( this.first() ) { // swap athletes
+				clubOptions = new Options( player.getAthletesAsSellables() ).join( "Cancel" );
+				this.output("Select an athlete to swap (Enter " + clubOptions.last() + " to leave)");
+				int athleteChoice1 = this.options(clubOptions);
+				
+				if (! this.last() ) { // choose a second athlete
+					clubOptions = new Options( player.getAthletesAsSellables() ).join( "Cancel" );
+					this.output("Select another athlete to swap (Enter " + clubOptions.last() + " to leave)");
+					int athleteChoice2 = this.options(clubOptions);
+					
+					if (! this.last() ) {
+						player.swapAthletes(athleteChoice1, athleteChoice2);
+					}
+				}
+			}
+			
+		} else if (this.first(1)) { // view inventory
+			this.output( player.getInventory() );
+			// use items
+			Options clubOptions = new Options( player.getInventory().getSellables() ).join( "Leave Inventory" );
+			this.output("Select an item to use (Enter " + clubOptions.last() + " to leave)");
+			int itemChoice = this.options(clubOptions);
+			
+			if (! this.last() ) {
+				clubOptions = new Options( player.getAthletesAsSellables() ).join( "Leave" );
+				this.output("Select an athlete to use it on (Enter " + clubOptions.last() + " to leave)");
+				int athleteChoice = this.options(clubOptions);
+				
+				if (! this.last() ) {
+					player.useItem(itemChoice, athleteChoice);
+				}
+			}
+		}
+	}
+	public void simulateStadium(Matches matches) {
+		while (true) {
+			Options staduimOptions = new Options( matches.getMatchesList() ).join( "Go back..." );
+			this.output("Matches left you can play");
+			int choice = this.options( staduimOptions);
+			if (choice == staduimOptions.last()) {
+				// leave the staduim page...
+				break;
+			}
+			if (! this.last()) {
+				Match match = matches.playMatch( choice ); // uses up the match
+				this.output( match.getResults() );
+			}
+		}
+	}
+	public void goToShop(Shop itemShop, Shop athleteShop, Team player) {
+		Options shopChoiceOptions = new Options(new String[] {"Item Shop", "Athlete Shop", "Leave Shop"} );
+		this.output("Select an option (Enter " + shopChoiceOptions.last() + " to leave)");
+		int choice = this.options(shopChoiceOptions);
+		
+		/*
+		if (this.last()) { // Leave shop
+			continue;					
+		}
+		*/
+		
+		Shop shop = new Shop(); // must initialize so java stays happy
+		
+		if (this.first()) { // Item shop
+			shop = itemShop;
+		} else if (this.first(1)) { // Athlete shop
+			shop = athleteShop;
+		}
+		
+		if (! this.last()) {
+			this.simulateShop( shop, player );
 		}
 	}
 	public static void main(String[] args) {
@@ -198,88 +284,18 @@ public class Game {
 				
 				
 				if (game.first()) { // (1) go to the club
-					Options clubChoiceOptions = new Options(new String[] {"View Team", "View Inventory", "Leave Club"} );
-					game.output("Select an option (Enter " + clubChoiceOptions.last() + " to leave)");
-					choice = game.options(clubChoiceOptions);
 					
-					if (game.first()) { // view team
-						game.output( player );
-						// swap athletes
-						Options clubOptions = new Options(new String[] {"Swap Athletes", "Leave Club"} );
-						game.output("Select an option (Enter " + clubOptions.last() + " to leave)");
-						choice = game.options(clubOptions);
-						
-						if ( game.first() ) { // swap athletes
-							clubOptions = new Options( player.getAthletesAsSellables() ).join( "Cancel" );
-							game.output("Select an athlete to swap (Enter " + clubOptions.last() + " to leave)");
-							int athleteChoice1 = game.options(clubOptions);
-							
-							if (! game.last() ) { // choose a second athlete
-								clubOptions = new Options( player.getAthletesAsSellables() ).join( "Cancel" );
-								game.output("Select another athlete to swap (Enter " + clubOptions.last() + " to leave)");
-								int athleteChoice2 = game.options(clubOptions);
-								
-								if (! game.last() ) {
-									player.swapAthletes(athleteChoice1, athleteChoice2);
-								}
-							}
-						}
-						
-					} else if (game.first(1)) { // view inventory
-						game.output( player.getInventory() );
-						// use items
-						Options clubOptions = new Options( player.getInventory().getSellables() ).join( "Leave Inventory" );
-						game.output("Select an item to use (Enter " + clubOptions.last() + " to leave)");
-						int itemChoice = game.options(clubOptions);
-						
-						if (! game.last() ) {
-							clubOptions = new Options( player.getAthletesAsSellables() ).join( "Leave" );
-							game.output("Select an athlete to use it on (Enter " + clubOptions.last() + " to leave)");
-							int athleteChoice = game.options(clubOptions);
-							
-							if (! game.last() ) {
-								player.useItem(itemChoice, athleteChoice);
-							}
-						}
-					}
+					game.simulateClub(player);
 					
-					//player.useItem(item, athlete);
-					// item objects has a function which takes athlete as a parameter then applies effects to athlete.
 					continue;
 				} else if (game.first(1)) { // (2) go to the statium
 				
-					while (true) {
-						Options staduimOptions = new Options( matches.getMatchesList() ).join( "Go back..." );
-						game.output("Matches left you can play");
-						choice = game.options( staduimOptions);
-						if (choice == staduimOptions.last()) {
-							// leave the staduim page...
-							break;
-						}
-						if (! game.last()) {
-							Match match = matches.playMatch( choice ); // uses up the match
-							game.output( match.getResults() );
-						}
-					}
+					game.simulateStadium(matches);
+					
 					continue;
 				} else if (game.first(2)) { // (3) visit the shop/market
-					Options shopChoiceOptions = new Options(new String[] {"Item Shop", "Athlete Shop", "Leave Shop"} );
-					game.output("Select an option (Enter " + shopChoiceOptions.last() + " to leave)");
-					choice = game.options(shopChoiceOptions);
 					
-					if (game.last()) { // Leave shop
-						continue;					
-					}
-					
-					//Shop shop = new Shop();
-					
-					if (game.first()) { // Item shop
-						shop = itemShop;
-					} else if (game.first(1)) { // Athlete shop
-						shop = athleteShop;
-					}
-					
-					game.simulateShop( shop, player );
+					game.goToShop(itemShop, athleteShop, player);
 					
 					continue;
 				} else if (game.last()) { // (4) move to the next week
@@ -288,6 +304,7 @@ public class Game {
 					// random events
 					// train athlete
 					game.resetShop(itemShop, athleteShop);
+					game.resetMatches(matches, player);
 					inWeek = false;
 					continue;
 				}
