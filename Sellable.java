ca1ac470ebcd03d774bed5bc22181ea1e270e57e
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:34:34 +1200

    Update Sellable.java

--- Sellable.java
+++ Sellable.java
@@ -1,5 +1,25 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Sellable.
+ */
 public interface Sellable {
+	
+	/**
+	 * Price.
+	 *
+	 * @return the money
+	 */
 	public Money price();
+	
+	/**
+	 * Gets the option.
+	 *
+	 * @return the option
+	 */
 	public String getOption();
+	
+	/**
+	 * Bought.
+	 */
 	public void bought();
 }
