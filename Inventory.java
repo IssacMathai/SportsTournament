92b0c6fe30cf88a187a3b7c0e2d0b123bce25503
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:31:43 +1200

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,13 +1,35 @@
 import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Inventory.
+ */
 public class Inventory {
+	
+	/** The items. */
 	private ArrayList<Item> items;
+	
+	/**
+	 * Instantiates a new inventory.
+	 */
 	public Inventory() {
 		this.items = new ArrayList<Item>();
 	}
+	
+	/**
+	 * Gets the.
+	 *
+	 * @return the array list
+	 */
 	public ArrayList<Item> get() {
 		return this.items;
 	}
+	
+	/**
+	 * Gets the sellables.
+	 *
+	 * @return the sellables
+	 */
 	public ArrayList<Sellable> getSellables() {
 		ArrayList<Sellable> asdf = new ArrayList<Sellable>();
 		for (Item item : this.items) {
@@ -15,23 +37,62 @@ public class Inventory {
 		}
 		return asdf;
 	}
+	
+	/**
+	 * Gets the.
+	 *
+	 * @param index the index
+	 * @return the item
+	 */
 	public Item get(int index) {
 		return this.items.get(index);
 	}
+	
+	/**
+	 * Adds the.
+	 *
+	 * @param item the item
+	 */
 	public void add(Item item) {
 		this.items.add(item);
 	}
+	
+	/**
+	 * Exists.
+	 *
+	 * @param index the index
+	 * @return true, if successful
+	 */
 	public boolean exists(int index) {
 		return 0 <= index && index < this.items.size();
 	}
+	
+	/**
+	 * Removes the.
+	 *
+	 * @param index the index
+	 */
 	public void remove(int index) {
 		this.items.remove(index);
 	}
+	
+	/**
+	 * Use.
+	 *
+	 * @param index the index
+	 * @return the item
+	 */
 	public Item use(int index) {
 		Item item = this.get(index);
 		this.remove(index);
 		return item;
 	}
+	
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	public String toString() {
 		String string = "Inventory\n";
 		for (int i = 0; i < this.items.size(); i++) {
