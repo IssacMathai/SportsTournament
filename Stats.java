8b0d5b75ea6f199553e74fde7358144e44ebecba
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:35:42 +1200

    Update Stats.java

--- Stats.java
+++ Stats.java
@@ -1,10 +1,32 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Stats.
+ */
 public class Stats {
+	
+	/** The stats. */
 	private int[] stats;
+	
+	/** The names. */
 	private String[] names = {"Accuracy", "Evasion", "Speed", "Strength", "Tough-skin"};
+	
+	/** The cols. */
 	private String[] cols = {"#a40", "#0a4", "#40a", "#a40", "#0a4", "#40a"};
+	
+	/**
+	 * Instantiates a new stats.
+	 *
+	 * @param stats the stats
+	 */
 	public Stats(int[] stats) {
 		this.stats = stats;
 	}
+	
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	public String toString() {
 		String string = "";
 		for (int i = 0; i < this.stats.length - 1; i++) {
@@ -15,9 +37,21 @@ public class Stats {
 		}
 		return string;
 	}
+	
+	/**
+	 * Gets the.
+	 *
+	 * @return the int[]
+	 */
 	public int[] get() {
 		return this.stats;
 	}
+	
+	/**
+	 * Apply.
+	 *
+	 * @param effectsStats the effects stats
+	 */
 	public void apply(Stats effectsStats) {
 		int[] effects = effectsStats.get();
 		for (int i = 0; i < this.stats.length; i++) {
