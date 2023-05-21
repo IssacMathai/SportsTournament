11ae79471a7a6a8c32582244ef5d9466985364d9
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:35:59 +1200

    Update Team.java

--- Team.java
+++ Team.java
@@ -1,13 +1,35 @@
 import java.util.ArrayList;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Team.
+ */
 public class Team {
+	
+	/** The name. */
 	private String name;
+	
+	/** The money. */
 	private Money money;
+	
+	/** The team size. */
 	private int teamSize;
+	
+	/** The field count. */
 	private int fieldCount;
+	
+	/** The athletes. */
 	private ArrayList<Athlete> athletes;
+	
+	/** The reserves. */
 	private ArrayList<Athlete> reserves;
+	
+	/** The inventory. */
 	private Inventory inventory;
+	
+	/**
+	 * Instantiates a new team.
+	 */
 	public Team() {
 		this.name = "";
 		this.money = new Money(0);
@@ -17,6 +39,12 @@ public class Team {
 		this.reserves = new ArrayList<Athlete>();
 		this.inventory = new Inventory();
 	}
+	
+	/**
+	 * Instantiates a new team.
+	 *
+	 * @param name the name
+	 */
 	public Team(String name) {
 		this.name = name;
 		this.money = new Money(0);
@@ -26,6 +54,12 @@ public class Team {
 		this.reserves = new ArrayList<Athlete>();
 		this.inventory = new Inventory();
 	}
+	
+	/**
+	 * Instantiates a new team.
+	 *
+	 * @param teamSize the team size
+	 */
 	public Team(int teamSize) {
 		this.name = "";
 		this.money = new Money(0);
@@ -35,6 +69,13 @@ public class Team {
 		this.reserves = new ArrayList<Athlete>();
 		this.inventory = new Inventory();
 	}
+	
+	/**
+	 * Instantiates a new team.
+	 *
+	 * @param name the name
+	 * @param teamSize the team size
+	 */
 	public Team(String name, int teamSize) {
 		this.name = name;
 		this.money = new Money(0);
@@ -44,6 +85,14 @@ public class Team {
 		this.reserves = new ArrayList<Athlete>();
 		this.inventory = new Inventory();
 	}
+	
+	/**
+	 * Instantiates a new team.
+	 *
+	 * @param name the name
+	 * @param teamSize the team size
+	 * @param fieldCount the field count
+	 */
 	public Team(String name, int teamSize, int fieldCount) {
 		this.name = name;
 		this.money = new Money(0);
@@ -53,6 +102,12 @@ public class Team {
 		this.reserves = new ArrayList<Athlete>();
 		this.inventory = new Inventory();
 	}
+	
+	/**
+	 * All injured.
+	 *
+	 * @return true, if successful
+	 */
 	public boolean allInjured() { // if all the first 5 athletes are injured
 		for (int i = 0; i < this.athletes.size(); i++) {
 			if (i == 5) {
@@ -64,35 +119,94 @@ public class Team {
 		}
 		return true; // they were all injured
 	}
+	
+	/**
+	 * Reset stamina.
+	 */
 	public void resetStamina() {
 		for (Athlete athlete : this.athletes) {
 			athlete.setStamina(100);
 		}
 	}
+	
+	/**
+	 * Sets the name.
+	 *
+	 * @param name the new name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	/**
+	 * Gets the name.
+	 *
+	 * @return the name
+	 */
 	public String getName() {
 		return this.name;
 	}
+	
+	/**
+	 * Sets the money.
+	 *
+	 * @param money the new money
+	 */
 	public void setMoney(Money money) {
 		this.money = money;
 	}
+	
+	/**
+	 * Sets the money.
+	 *
+	 * @param money the new money
+	 */
 	public void setMoney(int money) {
 		this.money = new Money(money);
 	}
+	
+	/**
+	 * Gets the money.
+	 *
+	 * @return the money
+	 */
 	public Money getMoney() {
 		return this.money;
 	}
+	
+	/**
+	 * Gets the team size.
+	 *
+	 * @return the team size
+	 */
 	public int getTeamSize() {
 		return this.teamSize;
 	}
+	
+	/**
+	 * Gets the field count.
+	 *
+	 * @return the field count
+	 */
 	public int getFieldCount() {
 		return this.fieldCount;
 	}
+	
+	/**
+	 * Team count.
+	 *
+	 * @return the int
+	 */
 	public int teamCount() {
 		return this.athleteCount() + this.reserveCount();
 	}
+	
+	/**
+	 * Adds the sellable.
+	 *
+	 * @param sellable the sellable
+	 * @return true, if successful
+	 */
 	public boolean addSellable(Sellable sellable) {
 		if (sellable instanceof Athlete) { // add athlete
 			return this.addAthlete( (Athlete) sellable );
@@ -101,23 +215,57 @@ public class Team {
 			return false;
 		}
 	}
+	
+	/**
+	 * Gets the inventory.
+	 *
+	 * @return the inventory
+	 */
 	public Inventory getInventory() {
 		return this.inventory;
 	}
+	
+	/**
+	 * Use item.
+	 *
+	 * @param item the item
+	 * @param athlete the athlete
+	 * @return the item
+	 */
 	public Item useItem(int item, int athlete) { // should use an item from the players inventory, then remove it
 		this.athletes.get(athlete).useItem( this.inventory.get(item) );
 		return this.inventory.get().remove(item);
 	}
+	
+	/**
+	 * Swap athletes.
+	 *
+	 * @param first the first
+	 * @param second the second
+	 */
 	public void swapAthletes(int first, int second) { // should swap 2 athletes
 		Athlete athlete1 = this.athletes.get(first);
 		Athlete athlete2 = this.athletes.get(second);
 		this.athletes.set(first, athlete2);
 		this.athletes.set(second, athlete1);
 	}
+	
+	/**
+	 * Athlete count.
+	 *
+	 * @return the int
+	 */
 	// Athlete Stuff
 	public int athleteCount() {
 		return this.athletes.size();
 	}
+	
+	/**
+	 * Adds the athlete.
+	 *
+	 * @param athlete the athlete
+	 * @return true, if successful
+	 */
 	// return true on success
 	public boolean addAthlete(Athlete athlete) { // might throw an exception ("Team full!")
 		if (!this.athletes.contains(athlete) && this.athletes.size() < this.teamSize) {
@@ -127,9 +275,21 @@ public class Team {
 			return false;
 		}
 	}
+    
+    /**
+     * Gets the athletes.
+     *
+     * @return the athletes
+     */
     public ArrayList<Athlete> getAthletes() {
 		return this.athletes;
 	}
+    
+    /**
+     * Gets the athletes as sellables.
+     *
+     * @return the athletes as sellables
+     */
     public ArrayList<Sellable> getAthletesAsSellables() {
 		ArrayList<Sellable> asdf = new ArrayList<Sellable>();
 		for (Athlete athlete : this.athletes) {
@@ -137,33 +297,90 @@ public class Team {
 		}
 		return asdf;
 	}
+	
+	/**
+	 * Gets the athlete.
+	 *
+	 * @param index the index
+	 * @return the athlete
+	 */
 	public Athlete getAthlete(int index) {
 		return this.athletes.get(index);
 	}
+	
+	/**
+	 * Removes the athlete.
+	 *
+	 * @param athlete the athlete
+	 */
 	public void removeAthlete(Athlete athlete) {
 		this.athletes.remove(athlete);
 	}
+	
+	/**
+	 * Removes the athlete.
+	 *
+	 * @param index the index
+	 * @return the athlete
+	 */
 	public Athlete removeAthlete(int index) {
 		return this.athletes.remove(index);
 	}
+	
+	/**
+	 * Reserve count.
+	 *
+	 * @return the int
+	 */
 	// Reserve Stuff
 	public int reserveCount() {
 		return this.reserves.size();
 	}
+	
+	/**
+	 * Adds the reserve.
+	 *
+	 * @param athlete the athlete
+	 */
 	public void addReserve(Athlete athlete) {
 		if (!this.reserves.contains(athlete) && this.reserves.size() < this.teamSize) {
 			this.reserves.add(athlete);
 		}
 	}
+	
+	/**
+	 * Gets the reserve.
+	 *
+	 * @param index the index
+	 * @return the reserve
+	 */
 	public Athlete getReserve(int index) {
 		return this.reserves.get(index);
 	}
+	
+	/**
+	 * Removes the reserve.
+	 *
+	 * @param athlete the athlete
+	 */
 	public void removeReserve(Athlete athlete) {
 		this.reserves.remove(athlete);
 	}
+	
+	/**
+	 * Removes the reserve.
+	 *
+	 * @param index the index
+	 */
 	public void removeReserve(int index) {
 		this.reserves.remove(index);
 	}
+	
+	/**
+	 * To string.
+	 *
+	 * @return the string
+	 */
 	// Other Stuff
 	@Override
 	public String toString() {
@@ -186,6 +403,13 @@ public class Team {
 		}
 		return string;
 	}
+	
+	/**
+	 * Match string.
+	 *
+	 * @param index the index
+	 * @return the string
+	 */
 	public String matchString(int index) {
 		String string = "";
 		
