254a2807a2829009b273b5bfd517486439f270d6
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:36:10 +1200

    Update Validator.java

--- Validator.java
+++ Validator.java
@@ -1,3 +1,15 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Validator.
+ */
 public interface Validator {
+	
+	/**
+	 * Validate.
+	 *
+	 * @param string the string
+	 * @return true, if successful
+	 * @throws Exception the exception
+	 */
 	public boolean validate(String string) throws Exception;
 }
