9e320c64f107288cb911805553ed99ab3f0455a2
Issac Mathai <82129993+IssacMathai@users.noreply.github.com>
2023-05-21 15:31:10 +1200

    Update IntValidator.java

--- IntValidator.java
+++ IntValidator.java
@@ -1,10 +1,33 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Class IntValidator.
+ */
 public class IntValidator implements Validator {
+	
+	/** The lower bound. */
 	private int lowerBound;
+	
+	/** The upper bound. */
 	private int upperBound;
+	
+	/**
+	 * Instantiates a new int validator.
+	 *
+	 * @param lowerBound the lower bound
+	 * @param upperBound the upper bound
+	 */
 	public IntValidator(int lowerBound, int upperBound) {
 		this.lowerBound = lowerBound;
 		this.upperBound = upperBound;
 	}
+	
+	/**
+	 * Validate.
+	 *
+	 * @param string the string
+	 * @return true, if successful
+	 * @throws Exception the exception
+	 */
 	public boolean validate(String string) throws Exception {
 		try {
 			int number = Integer.parseInt(string);
